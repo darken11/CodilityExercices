0271337810bbd9548a0ab5e3097fde229b367fee
rda <rabidarhnaj@yahoo.fr>
2022-05-15 20:12:37 +0200

    second Distinct solution

--- recup/src/Distinct.java
+++ recup/src/Distinct.java
@@ -3,10 +3,25 @@ import java.util.Arrays;
 
 
 public class Distinct {
-    public int solution(int[] A){
-
-      return  (int)Arrays.stream(A).distinct().count();
 
 
+    public int solution(int[] A) {
+        int nbrDistinct=0;
+        if (A.length > 0) {
+            nbrDistinct = 1;
+
+            Arrays.sort(A);
+            for (int i = 0; i < A.length - 1; i++) {
+                if (A[i] != A[i + 1]) {
+                    nbrDistinct++;
+                }
+            }
+
+
+
+        }
+        return nbrDistinct;
     }
+    //les deux solutions valable
+    // return  (int)Arrays.stream(A).distinct().count();
 }
